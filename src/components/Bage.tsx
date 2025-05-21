af7d52b3074a34ed5bbc52fa0f06f2788afbf25d
aleshka <nikolaenkooleksiy@gmail.com>
2025-05-21 21:46:30 +0300

    fix padding for Bage component

--- src/components/Bage.tsx
+++ src/components/Bage.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const Bage = ({ className, count }: Props) => {
   return (
     <span
-      className={`absolute bottom-4 left-5 -z-10 flex items-center justify-center rounded-full bg-blue-600 p-1 text-xs font-bold text-white ${className}`}>
+      className={`absolute bottom-4 left-5 -z-10 flex items-center justify-center rounded-full bg-blue-600 py-1 px-2 text-xs font-bold text-white ${className}`}>
       {count}
     </span>
   );
